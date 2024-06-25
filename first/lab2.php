1b572b9cc0019d2253ba672b1ec78df3aabf58c8
siam21457883 <ahmedislam.siam@gmail.com>
2024-06-25 15:17:56 +0600

    Add files via upload

--- first/lab2.php
+++ first/lab2.php
@@ -13,34 +13,31 @@
 <legend>General Information</legend>
 
 <tr>  <label for="fname">First name:</label>
-  <p><?php echo $_REQUEST['fname'];?></p>
+  <p><?php echo $_POST['fname'];?></p>
 <br>
  <label for="lname">Last name:</label>
-  <p><?php echo $_REQUEST['lname'];?></p>
+  <p><?php echo $_POST['lname'];?></p>
   <br>
 <br>
 
  <label for="gender">Gender:</label>
-  <input type="radio" id="male" name="gender" value="male" >
-  <label for="male">Male</label>
-  <input type="radio" id="female" name="gender" value="female">
-  <label for="female">Female</label><br>
+<p><?php echo $_POST['gender'];?></p><br>
 <br>
  
  <label for="faname">Father's name:</label>
-  <p><?php echo $_REQUEST['faname'];?></p>
+  <p><?php echo $_POST['faname'];?></p>
 
   
  <label for="moname">Mother's name:</label>
-  <p><?php echo $_REQUEST['moname'];?></p><br>
+  <p><?php echo $_POST['moname'];?></p><br>
  <br>
  <label for="btype">Blood Group:</label>
-<p><?php echo $_REQUEST['btype'];?></p><br>
+<p><?php echo $_POST['Btype'];?></p><br>
   <br>
   
   
   <label for="religion">Religion:</label>
-<p><?php echo $_REQUEST['religion'];?></p><br>
+<p><?php echo $_POST['religion'];?></p><br>
   <br>
   
 
@@ -55,13 +52,13 @@
 
 
   <label for="email">Email:</label>
-<p><?php echo $_REQUEST['email'];?></p>
+<p><?php echo $_POST['email'];?></p>
 <br>
  <label for="pnumber">Phone Number:</label>
-<p><?php echo $_REQUEST['pnumber'];?></p>
+<p><?php echo $_POST['pnumber'];?></p>
 <br>
  <label for="pnumber">Website:</label>
-  <p><?php echo $_REQUEST['Website'];?></p><br>
+  <p><?php echo $_POST['Website'];?></p><br>
 <br>
 
 <label for="paddress">Address:</label>
@@ -70,15 +67,15 @@
 <legend>Present Address</legend>
 <table>
 <tr>
-<p><?php echo $_REQUEST['country'];?></p></tr>
+<p><?php echo $_POST['country'];?></p></tr>
  <tr>
-<p><?php echo $_REQUEST['city'];?></p>
+<p><?php echo $_POST['city'];?></p>
  </tr><br><br>
  <tr>
- <p><?php echo $_REQUEST['address'];?></p>
+ <p><?php echo $_POST['address'];?></p>
  </tr><br>
   <tr>
- <p><?php echo $_REQUEST['postcode'];?></p>
+ <p><?php echo $_POST['postcode'];?></p>
  </tr><br>
 </table>
 </fieldset>
@@ -96,14 +93,14 @@
 
 
   <label for="username">Username:</label>
-  <p><?php echo $_REQUEST['username'];?></p>
+  <p><?php echo $_POST['username'];?></p>
 <br>
  <label for="password">Password:</label>
-  <p><?php echo $_REQUEST['password'];?></p>
+  <p><?php echo $_POST['password'];?></p>
 <br>
 
  <label for="cpassword">Confirm password:</label>
-  <p><?php echo $_REQUEST['cpassword'];?></p><br>
+  <p><?php echo $_POST['cpassword'];?></p><br>
 <br>
    
 
